a2392d3a5e938e9b2ecf4d15a685198c4d69be0f
ylu021 <yichen.luu@gmail.com>
2025-02-24 14:39:41 -0500

    add hello world activity

--- client/main.js
+++ client/main.js
@@ -1,7 +1,49 @@
-import './style.css'
-import rocketLogo from '/rocket.png'
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+import "./style.css";
+import rocketLogo from "/rocket.png";
 
-document.querySelector('#app').innerHTML = `
+let auth;
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authourize the SDK
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: ["identify", "guilds", "applications.commands"],
+  });
+
+  // retreive the access_token from the response
+  const response = await fetch("/.proxy/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ code }),
+  });
+
+  const { access_token } = await response.json();
+
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Failed to authenticate Discord SDK");
+  }
+}
+
+document.querySelector("#app").innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,13 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '../',
+  envDir: "../",
   server: {
+    allowedHosts: ["localhost", "413f-207-38-230-145.ngrok-free.app"],
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
         ws: true,
